7ae26e8457ea71183f46937ff2cb94142b92ee16
Marco Vintenove <marcovintenove@gmail.com>
2015-02-22 15:34:54 +0800

    Fix TextView bug on screen rotation It wasn't null but was pointing to wrong place

--- app/src/main/java/color/com/textcoloradapter/MainActivity.java
+++ app/src/main/java/color/com/textcoloradapter/MainActivity.java
@@ -2,6 +2,7 @@ package color.com.textcoloradapter;
 
 import android.app.WallpaperManager;
 import android.content.Context;
+import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
@@ -21,13 +22,15 @@ import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 
+    private static boolean LOG = (BuildConfig.DEBUG) ? true : false;
+    private static final String UNDERGROUND = "That is my conviction of forty years. I am forty years old now, and you know forty years is a whole lifetime; you know it is extreme old age. To live longer than forty years is bad manners, is vulgar, immoral. Who does live beyond forty? Answer that, sincerely and honestly I will tell you who do: fools and worthless fellows. I tell all old men that to their face, all these venerable old men, all these silver-haired and reverend seniors! I tell the whole world that to its face! I have a right to say so, for I shall go on living to sixty myself. To seventy! To eighty! ... Stay, let me take breath ... ";
     private static String mTextColor;
-    private static TextView mTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
@@ -39,16 +42,16 @@ public class MainActivity extends ActionBarActivity {
     protected void onPostResume() {
         super.onPostResume();
         long start = System.currentTimeMillis();
-        if (mTV != null) {
-            final Bitmap b = Utils.drawableToBitmap(WallpaperManager.getInstance(getApplicationContext()).getDrawable());
+        final TextView tv = (TextView) findViewById(R.id.texto);
+        final Bitmap b = Utils.drawableToBitmap(WallpaperManager.getInstance(getApplicationContext()).getDrawable());
 
-            mTextColor = Utils.getContrast50(Utils.getHexStringAverageColor(b)); // could use a different contrast function
+        mTextColor = Utils.getContrast50(Utils.getHexStringAverageColor(b)); // could use a different contrast function
 
-            mTV.setText("That is my conviction of forty years. I am forty years old now, and you know forty years is a whole lifetime; you know it is extreme old age. To live longer than forty years is bad manners, is vulgar, immoral. Who does live beyond forty? Answer that, sincerely and honestly I will tell you who do: fools and worthless fellows. I tell all old men that to their face, all these venerable old men, all these silver-haired and reverend seniors! I tell the whole world that to its face! I have a right to say so, for I shall go on living to sixty myself. To seventy! To eighty! ... Stay, let me take breath ... ");
-            if (mTextColor.equals(Utils.BLACK)) mTV.setTextColor(Color.BLACK);
-            else mTV.setTextColor(Color.WHITE);
-        }
-        //Log.d("MERDA", "" + (System.currentTimeMillis() - start));
+        tv.setText(UNDERGROUND);
+        if (mTextColor.equals(Utils.BLACK)) tv.setTextColor(Color.BLACK);
+        else tv.setTextColor(Color.WHITE);
+        if (LOG) Log.d("MERDA", "" + (System.currentTimeMillis() - start) + " " + tv.getText());
+
     }
 
     /**
@@ -63,8 +66,6 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            if (mTV == null)
-                mTV = (TextView) rootView.findViewById(R.id.texto);
 
             return rootView;
         }
